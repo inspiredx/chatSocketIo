8e4c209dc025df0b34ff08d61c8e59b0cf2108e6
Georgiy Kapustin <84815448+inspiredx@users.noreply.github.com>
2022-11-01 19:24:26 +0300

    Final commit

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ server.listen(3000);
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
+
+users = [];
+connections = [];
+
+io.sockets.on('connection', function(socket) {
+    console.log("Connected succesfully");
+    connections.push(socket);
+
+    socket.on('disconnect', function(data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Disconnected");
+    });
+
+    socket.on('Sending', function(data) {
+        io.sockets.emit('Adding message', {msg: data.msg, name: data.name})
+    })
+});

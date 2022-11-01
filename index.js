88905d484482a5e3e2bd4de64af8b44760a9ac53
Georgiy Kapustin <84815448+inspiredx@users.noreply.github.com>
2022-11-01 19:36:01 +0300

    Final commit

--- index.js
+++ index.js
@@ -5,23 +5,27 @@ var io = require('socket.io')(server);
 
 server.listen(3000);
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
 users = [];
 connections = [];
 
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
     console.log("Connected succesfully");
     connections.push(socket);
 
-    socket.on('disconnect', function(data) {
+    socket.on('disconnect', function (data) {
         connections.splice(connections.indexOf(socket), 1);
         console.log("Disconnected");
     });
 
-    socket.on('Sending', function(data) {
-        io.sockets.emit('Adding message', {msg: data.msg, name: data.name, className: data.className})
+    socket.on('Sending', function (data) {
+        io.sockets.emit('Adding message', {
+            msg: data.msg,
+            name: data.name,
+            className: data.className
+        })
     })
 });
